baff67de3d29fa2ddbd5df53e5b3755297a3eedd
Luke E. Holman <lukeearlholman@yahoo.co.uk>
2023-05-15 16:43:00 +0200

    organised script
    lots of little changes

--- 4.Analyses.Script.R
+++ 4.Analyses.Script.R
@@ -74,7 +74,7 @@ oceanResistance <- as.matrix(read.csv("distanceData/OceanogrphicResistanceMatrix
 
 # pull the data in 
 
-particle <- read.csv("ParticleTracking/TrialStats.csv")
+particle <- read.csv("ParticleTracking/ParticleSpread.csv")
 
 # make the colour index
 
@@ -106,7 +106,7 @@ fishAlpha <- data.frame("ID"=names(fishdatB),"Richness"= colSums(fishdatB))
 
 #build the model
 lm1 <- lm(fishAlpha$Richness~as.factor(metadatSites$EcoRegion[match(fishAlpha$ID,metadatSites$SiteID)]))
-
+summary(lm1)
 # are residuals norm distributed?
 shapiro.test(residuals(lm1))
 
@@ -158,7 +158,6 @@ dev.off()
 
 ####====4.0 Beta Diversity ====####
 
-
 ### What are the previously described ecoregions from 
 
 groups0 <- sapply(strsplit(colnames(fishdatSite),"\\."),"[",1)
@@ -170,7 +169,10 @@ groups2[groups0=="RED"]<-"Western"
 
 #ius the variance between ecoregions similar? -  lets run a PERMdisp 
 fishbetadisp <- betadisper(vegdist(t(fishdatSite), "jaccard",binary=TRUE),groups1)
+fishbetadisp <- betadisper(vegdist(t(fishdatSite), "bray"),groups1)
 anova(fishbetadisp)
+TukeyHSD(fishbetadisp)
+
 #Sig difference in multivariate homogeneity - probably due to different numbers of samples
 
 
@@ -180,9 +182,12 @@ adonis2(vegdist(t(fishdatSite), "jaccard",binary=TRUE)~groups1,permutations = 10
 ## quite uneven samples so lets test individually 
 
 adonis.pair(vegdist(t(fishdatSite), "jaccard",binary=TRUE),as.factor(groups1),nper = 1000)
+#adonis.pair(vegdist(t(fishdatSite), "bray"),as.factor(groups1),nper = 1000)
 
-
-## Elizabeth ecoreigon sinply doesnt have enough observations.......
+## Elizabeth bio region simply doesnt have enough observations....... here we run again with reps not averaged
+adonis.pair(vegdist(t(fishdat), "jaccard",binary=TRUE),
+            as.factor(metadatSites$EcoRegion[match(sapply(strsplit(colnames(fishdat),"\\."),"[",1),metadatSites$SiteID)]),
+            nper = 1000)
 
 
 
@@ -191,6 +196,7 @@ adonis.pair(vegdist(t(fishdatSite), "jaccard",binary=TRUE),as.factor(groups1),np
 pdf("figures/FishBetaDiv.pdf",height = 5,width = 6)
 par(mar=c(2.1, 2.1, 2.1, 2.1))
 nMDS <- metaMDS(vegdist(t(fishdat),method="jaccard",binary=TRUE),trymax=500)
+nMDS <- metaMDS(vegdist(t(fishdat),method="bray"),trymax=500)
 
 plot(nMDS$points[,1],nMDS$points[,2],
      pch=16,
@@ -225,8 +231,247 @@ text(nMDS$points[,1],nMDS$points[,2]+0.05,labels=colnames(fishdat),cex=0.3)
 
 
 
+####====5.0 Linking to Oceanography ====####
 
-####====5.0 Linking to Oceanogrphy ====####
+
+## Can we link biodiversity patterns to particle release parameters?
+
+# Let's subset the data to take only the -3 day data & match it up with the correct order of sites  
+particle3 <- particle[particle$day=="-3",]
+particle3o <- particle3[match(fishAlpha$ID,particle3$site),]
+
+# Lets run linear models to test for individual effects
+summary(lm(fishAlpha$Richness~particle3o$mean_spread))
+summary(lm(fishAlpha$Richness~particle3o$ave_dist))
+summary(lm(fishAlpha$Richness~particle3o$area..km.))
+summary(lm(fishAlpha$Richness~particle3o$mean_dist))
+#joint model
+summary(lm(fishAlpha$Richness~particle3o$mean_spread+
+             particle3o$ave_dist+
+             particle3o$area..km.+
+             particle3o$mean_dist))
+
+# Now beta diversity with a distance based redundancy analysis 
+
+m1 <- dbrda(vegdist(t(fishdatSite),method="jaccard",binary=TRUE)~particle3o$mean_spread+
+              particle3o$ave_dist+
+              particle3o$area..km.+
+              particle3o$mean_dist)
+
+plot(m1)
+anova(m1,permutations = 10000)
+anova(m1,by="margin",permutations = 10000)
+RsquareAdj(m1)
+
+
+
+# Let's start with alpha diversity 
+
+pdf("figures/FishRichnessToOceanography.pdf",width =9,height=7)
+par(mfrow=c(2,2))
+par(mar=c(4.1, 4.1, 2.1, 1.1))
+plot(particle3o$mean_spread,
+     fishAlpha$Richness,
+     ylab="Fish Richness",
+     xlab="Average Spread of Particles from Mean (Km)",
+     col=as.numeric(as.factor(metadatSites$island[match(fishAlpha$ID,metadatSites$SiteID)])),pch=16,cex=2.5)
+
+
+plot(particle3o$mean_dist,
+     fishAlpha$Richness,
+     ylab="Fish Richness",
+     xlab="Distance of Particle Centroid from Sampling Site (Km)",
+     col=as.numeric(as.factor(metadatSites$island[match(fishAlpha$ID,metadatSites$SiteID)])),pch=16,cex=2.5)
+
+plot(particle3o$area..km.,
+     fishAlpha$Richness,
+     ylab="Fish Richness",
+     xlab="Surface Area of Particles (Km)",
+     col=as.numeric(as.factor(metadatSites$island[match(fishAlpha$ID,metadatSites$SiteID)])),pch=16,cex=2.5)
+
+
+plot(particle3o$ave_dist,
+     fishAlpha$Richness,
+     ylab="Fish Richness",
+     xlab="Average Distance of Particles From Sampling Point (Km)",
+     col=as.numeric(as.factor(metadatSites$island[match(fishAlpha$ID,metadatSites$SiteID)])),pch=16,cex=2.5)
+
+dev.off()
+
+
+
+### Now beta dissimilarity 
+
+
+geographicDistance <- as.dist(as.matrix(read.csv("distanceData/SiteDistanceMatrix.csv",row.names = 1)),upper = T)
+oceanResistance <- as.dist(as.matrix(read.csv("distanceData/OceanogrphicResistanceMatrix.csv",row.names = 1)),upper = T)
+
+nMDS.eDNA <- metaMDS(vegdist(t(fishdatSite),method="jaccard",binary=TRUE),trymax=500)
+nMDS.dist <- metaMDS(geographicDistance,trymax=500)
+nMDS.resist <- metaMDS(oceanResistance,trymax=500)
+
+par(mfrow=c(1,3))
+plot(nMDS.eDNA,type = "t",main="eDNA")
+plot(nMDS.dist,type = "t",main="Distance")
+plot(nMDS.resist,type = "t",main="Resistance")
+
+par(mfrow=c(2,2))
+
+plot(geographicDistance,vegdist(t(fishdatSite),method="jaccard",binary=TRUE),pch=16,ylim=c(0,1))
+plot(oceanResistance,vegdist(t(fishdatSite),method="jaccard",binary=TRUE),pch=16)
+
+
+test <- myjac_mod(fishdatSite)
+test[test==0] <- NA
+
+plot(as.matrix(read.csv("distanceData/SiteDistanceMatrix.csv",row.names = 1)),test,ylim=c(0,1),pch=16)
+plot(as.matrix(read.csv("distanceData/OceanogrphicResistanceMatrix.csv",row.names = 1)),test,pch=16)
+
+
+plot(test,as.matrix(vegdist(t(fishdatSite),method="jaccard",binary=TRUE)))
+
+
+
+eDNAdistance.pair.mod = reshape2::melt(myjac_mod(fishdatSite), varnames=c("Start","End"))
+eDNAdistance.pair.mod.No0 <- eDNAdistance.pair.mod[-which(eDNAdistance.pair.mod$value == 0),]
+eDNAdistance.pair.No0 <- eDNAdistance.pair[-which(eDNAdistance.pair.mod$value == 0),]
+eDNAdistance.pair.bray.No0 <- eDNAdistance.pair.bray[-which(eDNAdistance.pair.mod$value == 0),]
+geographicDistance.pair.No0 <- geographicDistance.pair[-which(eDNAdistance.pair.mod$value == 0),]
+oceanResistance.pair.No0 <- oceanResistance.pair[-which(eDNAdistance.pair.mod$value == 0),]
+
+
+myjac = function (datamat) {
+  datamat = datamat>0
+  mj = apply(datamat, 2, function(x) {
+    apply(datamat, 2, function(y) {
+      return(sum(x&y)/sum(x|y))
+    })
+  })
+  return(1-mj)
+}
+
+myjac_mod = function (datamat) {
+  datamat = datamat>0
+  mj = apply(datamat, 2, function(x) {
+    apply(datamat, 2, function(y) {
+      return(sum(x&y)/sum(x))
+    })
+  })
+  return(1-mj)
+}
+
+
+
+plot(eDNAdistance.pair.mod.No0$value,eDNAdistance.pair.No0$value)
+
+plot(eDNAdistance.pair.mod.No0$value,geographicDistance.pair.No0$value)
+plot(eDNAdistance.pair.No0$value,geographicDistance.pair.No0$value)
+
+model1 = lm (eDNAdistance.pair.mod.No0$value~geographicDistance.pair.No0$value)
+model2 = lm (model1$residuals ~ oceanResistance.pair.No0$value)
+model3 = lm (eDNAdistance.pair.mod.No0$value~geographicDistance.pair.No0$value + oceanResistance.pair.No0$value)
+model4 = lm (eDNAdistance.pair.No0$value~geographicDistance.pair.No0$value + oceanResistance.pair.No0$value)
+model5 = lm (eDNAdistance.pair.bray.No0$value~geographicDistance.pair.No0$value + oceanResistance.pair.No0$value)
+
+
+abline(model1, col="firebrick", lwd=2)
+
+"dodgerblue4"
+
+summary(model1)
+summary(model2)
+summary(model3)
+summary(model4)
+
+
+##Predictions based from the model 
+
+y.eDNA <- eDNAdistance.pair.mod.No0$value
+x.GeoG <- geographicDistance.pair.No0$value
+z.OceR <- oceanResistance.pair.No0$value
+modelPredict = lm(y.eDNA~x.GeoG + z.OceR)
+
+predictedData <- data.frame("x.GeoG"=rep(seq(0,320000,1000),2),"z.OceR"= c(rep(0.38,321),rep(-0.38,321)))
+
+predictedData <- cbind(predictedData,predict(modelPredict,predictedData,se.fit = TRUE))
+predictedData$lwr <- predictedData$fit-1.96*predictedData$se.fit
+predictedData$upr <- predictedData$fit+1.96*predictedData$se.fit
+
+
+#Plot this model
+
+#RED = negative BLUE = positive 
+my_palette <- colorRampPalette(colors = c("red", "white","blue"))
+my_colours <- my_palette(100)
+
+pdf("figures/DistDecayV2.pdf",width = 9,height = 6.5)
+par(mar=c(4.1, 4.1, 2.1, 6.1))
+
+
+plot(geographicDistance.pair.No0$value/1000,
+     eDNAdistance.pair.mod.No0$value,
+     pch=16, cex=0.95, 
+     xlab="Geographic Distance (km)",
+     ylab="Jaccard Dissimilarity")
+
+
+polygon(x = c(predictedData$x.GeoG[predictedData$z.OceR==0.38],
+              rev(predictedData$x.GeoG[predictedData$z.OceR==0.38]))/1000,
+        y = c(predictedData$lwr[predictedData$z.OceR==0.38], 
+              rev(predictedData$upr[predictedData$z.OceR==0.38])),
+        col =  adjustcolor("blue", alpha.f = 0.10), border = NA)
+
+points(predictedData$x.GeoG[predictedData$z.OceR==0.38]/1000,
+       predictedData$fit[predictedData$z.OceR==0.38],
+       type="l",col=adjustcolor("blue", alpha.f = 0.30),lwd=2)
+
+polygon(x = c(predictedData$x.GeoG[predictedData$z.OceR==-0.38],
+              rev(predictedData$x.GeoG[predictedData$z.OceR==-0.38]))/1000,
+        y = c(predictedData$lwr[predictedData$z.OceR==-0.38], 
+              rev(predictedData$upr[predictedData$z.OceR==-0.38])),
+        col =  adjustcolor("red", alpha.f = 0.10), border = NA)
+
+
+points(predictedData$x.GeoG[predictedData$z.OceR==-0.38]/1000,
+       predictedData$fit[predictedData$z.OceR==-0.38],
+       type="l",col=adjustcolor("red", alpha.f = 0.30),lwd=2)
+
+points(geographicDistance.pair.No0$value/1000,
+       eDNAdistance.pair.mod.No0$value,
+       col=my_colours[findInterval(oceanResistance.pair.No0$value, seq(-0.38, 0.38, length.out = 100))],
+       pch=16,cex=0.8)
+
+
+legend_image <- as.raster(matrix(rev(my_palette(100)), ncol=1))
+rasterImage(legend_image, 330, 0.40, 340,0.6,xpd=T)
+text(x=333, y = seq(0.4,0.6,l=5), labels = paste0("-  ",seq(-0.38,0.38,l=5)),xpd=T,pos = 4,cex=0.7)
+
+
+dev.off()
+
+pdf("figures/DistDecayV1.pdf",width = 7,height = 5)
+par(mar=c(4.1, 4.1, 2.1, 6.1))
+
+plot((geographicDistance.pair.No0$value)/1000,
+     eDNAdistance.pair.No0$value,
+     pch=16, cex=0.95,
+     xlab="Geographic Distance (km)",
+     ylab="Jaccard Dissimilarity")
+
+
+points((geographicDistance.pair.No0$value)/1000,
+       eDNAdistance.pair.No0$value,
+       col=my_colours[findInterval(oceanResistance.pair.No0$value, seq(-0.38, 0.38, length.out = 100))],
+       pch=16,cex=0.8)
+
+legend_image <- as.raster(matrix(my_palette(100), ncol=1))
+rasterImage(legend_image, 350, 0.60, 360,0.75,xpd=T)
+text(x=353, y = seq(0.6,0.75,l=5), labels = paste0("-  ",seq(-0.38,0.38,l=5)),xpd=T,pos = 4,cex=0.7)
+
+dev.off()
+
+legend_image <- as.raster(matrix(my_palette(100), ncol=1))
+rasterImage(legend_image, 0, 0, 1,1,add=TRUE,xpd=T)
 
 
 
