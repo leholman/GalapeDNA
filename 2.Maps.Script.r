cce2a3229ce0b8ebb588b12e4262ba7133d01740
Luke E. Holman <lukeearlholman@yahoo.co.uk>
2023-05-30 18:50:23 -0600

    many maps many scripts

--- 2.Maps.Script.r
+++ 2.Maps.Script.r
@@ -9,7 +9,7 @@
 # Load packages
 library(ncdf4)
 library(raster)
-
+library(sp)
 
 ##Lots of lines hashed out here, original file is 10GB+ so only subset used ;) 
 #load GEBCO_2022 netcdf downloaded on 110822 - https://www.gebco.net/data_and_products/gridded_bathymetry_data/
@@ -50,6 +50,9 @@ colbr <- function(x, b1=50, b2=50, r1=-2, r2=-2) {
 
 galap.br <- colbr(gebco.crop,b2=1)
 
+galap.br[[3]][43] <- 10
+
+
 # Get country shapefiles
 eq <- getData("GADM", country="ECU", level=0)
 
@@ -60,7 +63,8 @@ blue.col <- colorRampPalette(c("darkblue", "lightblue"))
 
 pdf(width = 8,height=6.5,file="mapBuilding/test1.pdf")
 
-plot(gebco.crop, col=c(blue.col(galap.br[[1]]), grey.colors(galap.br[[2]])), breaks=galap.br[[3]],axes = FALSE,box=F,legend=F)
+plot(gebco.crop, col=c(blue.col(galap.br[[1]]+1), grey.colors(0)), breaks=galap.br[[3]],axes = FALSE,box=F,legend=F)
+plot(eq, add=TRUE,border=NA,col="grey34")
 
 #-92, -89, -1.55, 0.68)
 axis(1,
@@ -72,8 +76,8 @@ axis(2,
 
 
 #plot(eq, add=TRUE)
-points(metadata$lon2,metadata$lat2,pch=16, col='black',cex=1)
-points(metadata$lon2,metadata$lat2,pch=16, col='white',cex=0.5)
+#points(metadata$lon2,metadata$lat2,pch=16, col='black',cex=1)
+#points(metadata$lon2,metadata$lat2,pch=16, col='white',cex=0.5)
 
 dev.off()
 
@@ -103,8 +107,9 @@ ColIndex$Colour[match(paste0(metadata$EcoRegion,"-",metadata$island),ColIndex$Ec
 
 pdf(width = 8,height=6.35,file="mapBuilding/mapV1.pdf")
 
-plot(gebco.crop, col=c(blue.col(galap.br[[1]]), grey.colors(galap.br[[2]])), breaks=galap.br[[3]],axes = FALSE,box=F,legend=F)
-#plot(eq, add=TRUE)
+plot(gebco.crop, col=c(blue.col(galap.br[[1]]+1), grey.colors(0)), breaks=galap.br[[3]],axes = FALSE,box=F,legend=F)
+plot(eq, add=TRUE,border=NA,col="grey34")
+#plot(gebco.crop, col=c(blue.col(galap.br[[1]]), grey.colors(galap.br[[2]])), breaks=galap.br[[3]],axes = FALSE,box=F,legend=F)
 points(metadata$lon2,metadata$lat2,pch=16, col='white',cex=2.5)
 points(metadata$lon2,metadata$lat2,pch=16, col=ColIndex$Colour[match(paste0(metadata$EcoRegion,"-",metadata$island),ColIndex$EcoIsland)]
 ,cex=2)
@@ -127,6 +132,60 @@ plot(gebco.crop.mod, legend.only=TRUE, col=c(blue.col(galap.br[[1]]+1), grey.col
 
 dev.off()
 
+### Plots per species 
+
+datasetsharks <- dataset[dataset$Assign.Category=="G"&dataset$B.class=="Chondrichthyes",69:86]
+
+
+
+species <- "Spheniscus"
+
+
+
+
+#dataset <- read.csv("cleandata/Cleaned_Fish_wTAX.csv",row.names = 1)
+dataset <- read.csv("cleandata/Cleaned_Fish_wTAX.csv",row.names = 1)
+
+
+dataset <- dataset[dataset$Assign.Category=="G",]
+
+
+datasetBinary <- dataset
+datasetBinary[datasetBinary>0] <- 1
+datasetBinaryNoTax <- datasetBinary[,1:69]
+
+
+plottingdata <- data.frame(matrix(NA,nrow=length(datasetBinary[,1]),ncol=length(metadata$SiteID)))
+colnames(plottingdata) <- metadata$SiteID
+row.names(plottingdata) <- row.names(datasetBinary)
+
+for (column in 1:length(plottingdata[1,])){
+  plottingdata[,column] <- rowSums(datasetBinaryNoTax[,gsub("(^.*)[.][0-9]$","\\1",colnames(datasetBinaryNoTax)) %in% colnames(plottingdata)[column]])
+}
+
+
+
+for (species in dataset$Assign.Assigment){
+
+speciesplottingdata <- plottingdata[match(species,dataset$Assign.Assigment),]
+
+pdf(width = 8,height=6.35,file=paste0("mapBuilding/species/map_",species,".pdf"))
+plot(gebco.crop, col=c(rep("white",42), grey.colors(galap.br[[2]])), breaks=galap.br[[3]],axes = FALSE,box=F,legend = FALSE)
+points(metadata$lon2,metadata$lat2,pch=16, col='black',cex=1.5)
+points(metadata$lon2,metadata$lat2,pch=16, col="white")
+axis(1,
+     at=pretty(c(-92, -89)),
+     labels = parse(text=degreeLabelsEW(pretty(c(-92, -89)))),lwd = 0, lwd.ticks = 1)
+axis(2,
+     at=pretty(c(-1.55, 0.68)),
+     labels = parse(text=degreeLabelsNS(pretty(c(-1.55, 0.68)))),las=TRUE,lwd = 0, lwd.ticks = 1)
+points(metadata$lon2,metadata$lat2,pch=16, cex=t(speciesplottingdata)*1.2,col='black')
+points(metadata$lon2,metadata$lat2,pch=16, cex=t(speciesplottingdata),col='darkorange')
+#text(-90,0.5,labels=species,cex=0.8)
+
+dev.off()
+
+}
 
 
 
@@ -145,12 +204,16 @@ text(metadata$lon2,metadata$lat2+0.08,labels=metadata$SiteID,cex=0.5,col="green"
 site <- "RED"
 site <- "SUAR"
 
+pdf("mapBuilding/example.pdf",width=7,height=5)
+plot(gebco.crop, col=c(blue.col(galap.br[[1]]), grey.colors(galap.br[[2]])), breaks=galap.br[[3]],axes = FALSE,box=F,legend = FALSE)
+points(metadata$lon2,metadata$lat2,pch=16, col='black',cex=1)
+points(metadata$lon2,metadata$lat2,pch=16, col='white',cex=0.5)
 points(pathPointsTable$lon[pathPointsTable$Start==site],
        pathPointsTable$lat[pathPointsTable$Start==site],
-       col="lightgreen",
+       col="darkred",
        pch=16,
        cex=0.2)
-
+dev.off()
 
 
 
